7a156fc92e144d2589c72056b58a3af8514af667
vaerjngiar <vaerjngiar@gmail.com>
2018-02-21 16:14:02 +0200

    CLI task

---
+++ task/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"gopherexercisies/task/cmd"
+	"fmt"
+	"os"
+	homedir "github.com/mitchellh/go-homedir"
+	"path/filepath"
+	"gopherexercisies/task/db"
+)
+
+func main() {
+	home, _ := homedir.Dir()
+	dbPath := filepath.Join(home, "tasks.db")
+	must(db.Init(dbPath))
+	must(cmd.RootCmd.Execute())
+}
+
+func must(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+}
